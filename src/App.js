9250fab7c939a3104d35c858c45e5fad3709fe63
Alys9999 <74774427+Alys9999@users.noreply.github.com>
2024-04-10 00:14:21 -0700

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const formRef = useRef(null);
+  const [file, setFile] = useState(null);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  };
+    if (!file) {
+        alert('Please select a file first.');
+        return;
+    }
+s
+    try {
+        const presignResponse = await axios.post('https://1ie9pc397c.execute-api.us-east-1.amazonaws.com/prod/', {
+            fileName: file.name,
+            fileType: file.type,
+        });
 
+        const { url } = presignResponse.data;
 
+        await axios.put(url, file, {
+            headers: {
+                'Content-Type': file.type,
+            },
+        });
+
+        alert('File successfully uploaded!');
+    } catch (error) {
+        console.error('Upload failed:', error);
+        alert('Upload failed. Please try again.');
+    }
+};
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]); 
+};
+  
+  
+  
+  
+  
   const centerStyle = {
     display: 'flex',
     justifyContent: 'center',
@@ -25,7 +58,7 @@ function App() {
         </div>
         <div style={{ marginBottom: '20px' }}>
           <label htmlFor="fileInput" style={{ display: 'block', marginBottom: '5px' }}>File Input:</label>
-          <input type="file" id="fileInput" name="fileInput" required style={{ width: '100%' }} />
+          <input value={file} onChange={handleFileChange} type="file" id="fileInput" name="fileInput" required style={{ width: '100%' }} />
         </div>
         <button type="submit" style={{ cursor: 'pointer' }}>Submit</button>
       </form>
